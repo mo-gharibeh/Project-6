6cc285833dbf50b8a5eae15b13cb0fe2a5a8a7fa
hmoodeh98 <hmoodeh9820@gmail.com>
2024-08-17 23:49:25 +0300

    E-commerce

---
+++ E_commerce/E_commerce/Controllers/CartItemsController.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using E_commerce.Models;
+
+namespace E_commerce.Controllers
+{
+    public class CartItemsController : Controller
+    {
+        private E_commerceEntities db = new E_commerceEntities();
+
+        // GET: CartItems
+        public ActionResult Index()
+        {
+            var cartItems = db.CartItems.Include(c => c.Cart).Include(c => c.Product);
+            return View(cartItems.ToList());
+        }
+
+        public ActionResult Cart(int? id)
+        {
+            // افترض أنك تحصل على UserID من الجلسة أو من مصدر آخر
+            //int userId = ...;  وضع الكود المناسب للحصول على UserID للمستخدم الحالي
+
+            // الحصول على السلة والعناصر المرتبطة بها
+            var cartItems = db.Carts
+                              .Where(c => c.UserID == id)
+                              .SelectMany(c => c.CartItems)
+                              .Include(ci => ci.Product) // جلب تفاصيل المنتج
+                              .ToList();
+
+            return View(cartItems);
+        }
+
+        public ActionResult Create(int id , int userID)
+        {
+
+            // افترض أنك تحصل على UserID من الجلسة أو من مصدر آخر
+            int userId = userID; // وضع الكود المناسب للحصول على UserID للمستخدم الحالي
+
+            // تحقق مما إذا كان المستخدم يمتلك سلة بالفعل
+            var cart = db.Carts.SingleOrDefault(c => c.UserID == userId);
+
+            // إذا لم يكن لديه سلة، أنشئ واحدة جديدة
+            if (cart == null)
+            {
+                cart = new Cart
+                {
+                    UserID = userId
+                };
+                db.Carts.Add(cart);
+                db.SaveChanges(); // حفظ السلة الجديدة
+            }
+
+            // تحقق مما إذا كان العنصر موجود بالفعل في السلة
+            var existingCartItem = db.CartItems
+                                     .SingleOrDefault(ci => ci.CartID == cart.CartID && ci.ProductID == id);
+
+            if (existingCartItem != null)
+            {
+                // إذا كان العنصر موجوداً بالفعل، قم بزيادة الكمية
+                existingCartItem.Quantity++;
+            }
+            else
+            {
+                // إذا كان العنصر غير موجود في السلة، قم بإضافته
+                var cartItem = new CartItem
+                {
+                    CartID = cart.CartID,
+                    ProductID = id,
+                    Quantity = 1 // تبدأ الكمية من 1
+                };
+                db.CartItems.Add(cartItem);
+            }
+
+            db.SaveChanges(); // حفظ التغييرات
+
+            // إعادة التوجيه إلى صفحة السلة أو البقاء في نفس الصفحة
+            return RedirectToAction("Shop", "Products");
+        }
+
+        public ActionResult UpdateQuantity(int id, int quantity)
+        {
+            var cartItem = db.CartItems.Find(id);
+
+            if (cartItem != null && quantity > 0)
+            {
+                cartItem.Quantity = quantity;
+                db.SaveChanges();
+            }
+            else if (cartItem != null && quantity == 0)
+            {
+                db.CartItems.Remove(cartItem);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Cart", new { id = Session["UserID"] /*cartItem.Cart.UserID*/ });
+        }
+
+        public ActionResult Remove(int id)
+        {
+            var cartItem = db.CartItems.Find(id);
+
+            if (cartItem != null)
+            {
+                db.CartItems.Remove(cartItem);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Cart", new { id = Session["UserID"] /*cartItem.Cart.UserID*/ });
+        }
+
+
+     /// <summary>
+     /// ///////////////////////////
+     /// </summary>
+     /// <param name="id"></param>
+     /// <returns></returns>
+
+        // GET: CartItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CartItem cartItem = db.CartItems.Find(id);
+            if (cartItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Create
+        //public ActionResult Create()
+        //{
+        //    ViewBag.CartID = new SelectList(db.Carts, "CartID", "CartID");
+        //    ViewBag.ProductID = new SelectList(db.Products, "ProductID", "ProductName");
+        //    return View();
+        //}
+
+        // POST: CartItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "CartItemID,CartID,ProductID,Quantity")] CartItem cartItem)
+        //{
+
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.CartItems.Add(cartItem);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    ViewBag.CartID = new SelectList(db.Carts, "CartID", "CartID", cartItem.CartID);
+        //    ViewBag.ProductID = new SelectList(db.Products, "ProductID", "ProductName", cartItem.ProductID);
+        //    return View(cartItem);
+        //}
+
+
+
+
+        // GET: CartItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CartItem cartItem = db.CartItems.Find(id);
+            if (cartItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CartID = new SelectList(db.Carts, "CartID", "CartID", cartItem.CartID);
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "ProductName", cartItem.ProductID);
+            return View(cartItem);
+        }
+
+        // POST: CartItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CartItemID,CartID,ProductID,Quantity")] CartItem cartItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cartItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CartID = new SelectList(db.Carts, "CartID", "CartID", cartItem.CartID);
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "ProductName", cartItem.ProductID);
+            return View(cartItem);
+        }
+
+        // GET: CartItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CartItem cartItem = db.CartItems.Find(id);
+            if (cartItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cartItem);
+        }
+
+        // POST: CartItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CartItem cartItem = db.CartItems.Find(id);
+            db.CartItems.Remove(cartItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ E_commerce/E_commerce/Controllers/UserController.cs
@@ -0,0 +1,181 @@
+﻿using E_commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_commerce.Controllers
+{
+    public class UserController : Controller
+    {
+        public E_commerceEntities db = new E_commerceEntities();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(FormCollection form)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                string fullName = form["fullName"];
+                string userName = form["username"];
+                string password = form["password"];
+                string email = form["email"];
+                string conPassword = form["confirmPassword"];
+
+                var user = new User
+                {
+                    Username = userName,
+                    Email = email,
+                    Password = password,
+                    FullName = fullName
+
+                };
+
+
+                if (conPassword == password) {
+
+                    db.Users.Add(user);
+                    db.SaveChanges();
+
+                    var cart = new Cart
+                    {
+                        UserID = user.UserID // استخدام UserID من المستخدم الجديد
+                    };
+
+                    // إضافة السلة الجديدة إلى جدول Carts
+                    db.Carts.Add(cart);
+                    db.SaveChanges(); // حفظ التغييرات
+                    //ModelState.AddModelError("PasswordMismatch", "Succesfull Register");
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    ModelState.AddModelError("PasswordMismatch", "Passwords do not match.");
+                    return View();
+                }
+               
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session["name"] = "1";
+            return RedirectToAction("Home", "Category");
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection form)
+        {
+            string username = form["Username"];
+            string password = form["Password"];
+
+            var user = db.Users.FirstOrDefault(u => u.Username == username);
+            if (user == null)
+            {
+                ModelState.AddModelError("UserNotFound", "User not found.");
+                return View();
+            }
+
+            if (password == user.Password)
+            {
+                Session["name"] = "0";
+                Session["UserID"] = user.UserID;
+                return RedirectToAction("Home", "Categories");
+            }
+            else
+            {
+                ModelState.AddModelError("PasswordMismatch", "Invalid username or password.");
+                return View();
+            }
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
